bdce2be076bd58617d192db3e845c53be0fbb75a
Steve Todd <stodd@mashbo.com>
2022-01-25 12:37:19 +0000

    Updated type annotation

--- src/Support/PageObjects/PageObject.php
+++ src/Support/PageObjects/PageObject.php
@@ -33,7 +33,7 @@ abstract class PageObject
     }
 
     /**
-     * @param array<string, string> $args
+     * @param array $args
      */
     public function submitSlideoutForm(string $selector, string $prefix, array $args = []): static
     {
