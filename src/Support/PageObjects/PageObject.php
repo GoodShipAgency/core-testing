1ecfb8dd5d228c445b5827ae5f22edc08911d510
Steve Todd <stodd@mashbo.com>
2024-02-07 12:49:44 +0000

    Added support for Zenstruck Browser

--- src/Support/PageObjects/PageObject.php
+++ src/Support/PageObjects/PageObject.php
@@ -7,18 +7,19 @@ namespace Mashbo\CoreTesting\Support\PageObjects;
 use Mashbo\CoreTesting\Support\ElementObjects\FormElement;
 use Symfony\Bundle\FrameworkBundle\KernelBrowser;
 use Symfony\Component\BrowserKit\AbstractBrowser;
+use Zenstruck\Browser;
 
 abstract class PageObject
 {
-    protected AbstractBrowser $browser;
+    protected AbstractBrowser|Browser $browser;
     protected string $path = '';
 
-    public function __construct(AbstractBrowser $client)
+    public function __construct(AbstractBrowser|Browser $client)
     {
         $this->browser = $client;
     }
 
-    protected function getBrowser(): AbstractBrowser
+    protected function getBrowser(): AbstractBrowser|Browser
     {
         return $this->browser;
     }
